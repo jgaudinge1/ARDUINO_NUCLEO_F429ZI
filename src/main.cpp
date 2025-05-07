890892b48bb51afdac3db93eeae20eb42fe8a261
jgaudinge1 <jonathan.gaudin@etu-upsaclay.fr>
2025-05-07 10:17:08 +0200

    Compteur avance TP3

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,53 @@
 #include <C12832.h>
 #include "my_header.h"
 
+#define button digitalRead(PC13)
+
+static int cpt_appui = 0;
+
+void TaskCpt(void *)
+{
+    typedef enum {relache, attente_appui, appui, attente_relache} etat_type;
+    static etat_type etat =relache;
+    pinMode(PC13, INPUT_PULLDOWN);
+
+
+    while(true)
+    {
+        
+        switch(etat)
+        {
+            case relache:
+                etat = attente_appui;
+                break;
+
+            case attente_appui:
+                if(button)
+                {
+                    etat= appui;
+                }
+                break;
+
+            case appui:
+                cpt_appui++;
+                etat = attente_relache;
+                break;
+            
+            case attente_relache:
+                if(!button)
+                {
+                    etat = relache;
+                }
+                break;
+
+            default: break;
+
+        }
+
+        vTaskDelay(100/portTICK_PERIOD_MS);
+    }
+}
+
 void my_main(void *)
 {
     int j = 0;
@@ -17,18 +64,21 @@ void my_main(void *)
         lcd_cls();
 
         lcd_locate(0, 10);
-        lcd_printf("Mbed App Shield!");
+        lcd_printf("Compteur avance");
 
         lcd_locate(0, 20);
-        lcd_printf("Counting : %d", j++);
+        lcd_printf("Compteur : %d", cpt_appui);
 
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        vTaskDelay(200 / portTICK_PERIOD_MS);
     }
 }
 
 void setup()
 {
+ 
     xTaskCreate(my_main, "my_main", 1024, NULL, 1, NULL);
+
+    xTaskCreate(TaskCpt, "TaskCpt", 1024, NULL, 1, NULL);
 
     // Start the scheduler
     vTaskStartScheduler();
