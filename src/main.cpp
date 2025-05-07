d38a6284218ee149ff431e51043b4b8436aba795
jgaudinge1 <jonathan.gaudin@etu-upsaclay.fr>
2025-05-07 09:44:10 +0200

    TP Clignotement

--- src/main.cpp
+++ src/main.cpp
@@ -5,6 +5,43 @@
 #include <C12832.h>
 #include "my_header.h"
 
+#define red PB14
+#define green PB0
+#define blue PB7
+
+void TaskLed1(void *)
+{
+  while(true)
+  {
+    digitalToggle(green);
+    vTaskDelay(250/ portTICK_PERIOD_MS);
+    digitalToggle(green);
+  vTaskDelay(250/ portTICK_PERIOD_MS);
+  }
+}
+
+void TaskLed2(void *)
+{
+  while(true)
+  {
+    digitalToggle(blue);
+    vTaskDelay(500/ portTICK_PERIOD_MS);
+    digitalToggle(blue);
+    vTaskDelay(500/ portTICK_PERIOD_MS);
+  }
+}
+
+void TaskLed3(void *)
+{
+  while(true)
+  {
+    digitalToggle(red);
+    vTaskDelay(1000/ portTICK_PERIOD_MS);
+    digitalToggle(red);
+    vTaskDelay(1000/ portTICK_PERIOD_MS);
+  }
+}
+
 void my_main(void *)
 {
     int j = 0;
@@ -28,7 +65,21 @@ void my_main(void *)
 
 void setup()
 {
+
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+  digitalWrite(red, 0);
+  digitalWrite(green, 0);
+  digitalWrite(blue, 0);
+
     xTaskCreate(my_main, "my_main", 1024, NULL, 1, NULL);
+
+    xTaskCreate(TaskLed1, "TaskLed1", 1024, NULL, 1, NULL);
+
+    xTaskCreate(TaskLed2, "TaskLed2", 1024, NULL, 1, NULL);
+
+    xTaskCreate(TaskLed3, "TaskLed3", 1024, NULL, 1, NULL);
 
     // Start the scheduler
     vTaskStartScheduler();
