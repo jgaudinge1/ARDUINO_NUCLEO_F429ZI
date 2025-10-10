30b2a40d94c0b2d9333fc71940af96993b05c65e
jgaudinge1 <jonathan.gaudin@etu-upsaclay.fr>
2025-10-10 14:00:49 +0200

    WOW

---
+++ include/mp3.h
@@ -0,0 +1,9 @@
+//  Copyright (c) 2024 Antoine Tran Tan
+//
+
+#ifndef MP3_H
+#define MP3_H
+
+extern const char gandalf_sax_mp3_compressed[29926];
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -4,37 +4,96 @@
 #include <STM32FreeRTOS.h>
 #include <C12832.h>
 #include "my_header.h"
+#include "SPI.h"
+#include "event_groups.h"
 
+
+// Groupe d'événements pour la synchronisation entre tâches et interruptions
+EventGroupHandle_t eflags;
+
+// Instance de la classe SPI
+SPIClass my_spi;
+
+
+// Fonction appelée lors de l'interruption sur PF8
+void my_interrupt(void)
+{
+    // Signale l'événement à la tâche principale
+    xEventGroupSetBitsFromISR(eflags, 0x01, NULL);
+    // Petite temporisation pour éviter les rebonds
+    vTaskDelay(8/portTICK_PERIOD_MS);
+}
+
+
+// Tâche principale exécutée par FreeRTOS
 void my_main(void *)
 {
-    int j = 0;
+    uint32_t flags;
 
-    // Setup the LCD display
+    // Tableaux de paquets à transmettre via SPI
+    char paquet_tx[4][4] = {
+        {0x02, 0x00, 0x08, 0x00},
+        {0x02, 0x02, 0x7A, 0x00},
+        {0x02, 0x03, 0x20, 0x00},
+        {0x02, 0x0B, 0x2F, 0x2F}
+    };
+
+    // Transmission des paquets SPI avec synchronisation sur l'interruption
+    for(int i = 0; i < 4; i++)
+    {
+        digitalWrite(PE4, LOW); // Sélection du périphérique SPI
+        my_spi.transfer(paquet_tx, 4, SPI_TRANSMITONLY); // Transmission du paquet
+        flags = xEventGroupWaitBits(eflags, 0x01, pdTRUE, pdFALSE, -1); // Attente de l'interruption
+        digitalWrite(PE4, HIGH); // Désélection du périphérique
+    }
+
+    vTaskDelay(1000/portTICK_PERIOD_MS); // Attente avant d'initialiser l'écran
+
+    // Initialisation de l'écran LCD
     lcd_begin();
 
+    // Boucle d'affichage sur l'écran LCD
     while (true)
     {
-        lcd_cls();
-
-        lcd_locate(0, 10);
-        lcd_printf("Mbed App Shield!");
-
-        lcd_locate(0, 20);
-        lcd_printf("Counting : %d", j++);
-
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        lcd_cls(); // Efface l'écran
+        lcd_locate(0, 10); // Positionne le curseur
+        lcd_printf("TP4 Gaudin Jonathan"); // Affiche le texte
+        vTaskDelay(1000 / portTICK_PERIOD_MS); // Rafraîchissement toutes les secondes
     }
 }
 
+
+// Fonction d'initialisation Arduino
 void setup()
 {
+    // Configuration des broches
+    pinMode(PE4, OUTPUT); // Chip select SPI
+    digitalWrite(PE4, HIGH); // Désélection initiale
+    pinMode(PD12, OUTPUT); // Broche supplémentaire
+    digitalWrite(PD12, HIGH);
+    pinMode(PF8, INPUT); // Broche d'interruption
+
+    // Attache l'interruption sur PF8
+    attachInterrupt(PF8, my_interrupt, RISING);
+
+    // Configuration SPI
+    SPISettings settings(100000, MSBFIRST, SPI_MODE0);
+    my_spi.begin();
+    my_spi.beginTransaction(settings);
+
+    // Création du groupe d'événements pour la synchronisation
+    eflags = xEventGroupCreate();
+
+    // Création de la tâche principale
     xTaskCreate(my_main, "my_main", 1024, NULL, 1, NULL);
 
-    // Start the scheduler
+    // Démarrage du scheduler FreeRTOS
     vTaskStartScheduler();
 }
 
+
+// Boucle principale Arduino (non utilisée ici)
 void loop()
 {
-    // Empty. Things are done in my_main
+    // Vide. Tout est géré dans la tâche my_main
 }
